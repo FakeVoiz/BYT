0a8f436c48440a425cd31183e623cc08e51ca4e8
Viktor Lishchuk <91957137+FakeVoiz@users.noreply.github.com>
2021-12-03 19:33:56 +0100

    task-6-7 (4)
    -Account tests were added
    -Bank tests were added

--- individual-hw/task-6-7/test/b_Money/AccountTest.java
+++ individual-hw/task-6-7/test/b_Money/AccountTest.java
@@ -13,14 +13,15 @@ public class AccountTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		SEK = new Currency("SEK", 0.15);
+		DKK = new Currency("DKK", .2);
+		SEK = new Currency("SEK", .15);
 		SweBank = new Bank("SweBank", SEK);
 		SweBank.openAccount("Alice");
 
 		testAccountA = new Account("Hans", SEK);
-		testAccountA.deposit(new Money(10000000, SEK));
+		testAccountA.deposit(new Money(1000000, SEK));
 		testAccountB = new Account("Alice", SEK);
-		testAccountB.deposit(new Money(10000000, SEK));
+		testAccountB.deposit(new Money(1000000, SEK));
 
 		SweBank.deposit("Alice", new Money(1000000, SEK));
 	}
@@ -38,17 +39,53 @@ public class AccountTest {
 	}
 	
 	@Test
-	public void testTimedPayment() throws AccountDoesNotExistException {
-		fail("Write test case here");
+	public void testTimedPayment() {
+//		Creating payments for accountB with transition to accountA
+		testAccountB.addTimedPayment("0",0,1,new Money(1389,SEK),Nordea,testAccountA.getName());
+		testAccountB.addTimedPayment("1",0,2,new Money(2378,SEK),SweBank,testAccountA.getName());
+
+//		Checking if payments were created
+		assertTrue(testAccountB.timedPaymentExists("0"));
+		assertTrue(testAccountB.timedPaymentExists("1"));
+
+//		Removing first Payment
+		testAccountB.removeTimedPayment("0");
+//		Checking if first payment was removed
+		assertFalse(testAccountB.timedPaymentExists("0"));
+//		Checking if second payment exist
+		assertTrue(testAccountB.timedPaymentExists("1"));
 	}
 
 	@Test
 	public void testAddWithdraw() {
-		fail("Write test case here");
+//		Creating money that will be used for account manipulation
+		var dkk100Money = new Money(100,DKK);
+		var dkk200Money = new Money(200,DKK);
+		var sek1000Money = new Money(1000,SEK);
+
+//		Checking if account contains money that were deposited in setUp function
+		assertEquals(Double.valueOf(1000000),testAccountA.getBalance().getAmount());
+//		Withdrawing money with different currency
+		testAccountA.withdraw(dkk100Money);
+		assertEquals(Double.valueOf(999867),testAccountA.getBalance().getAmount());
+//		Depositing money with different currency
+		testAccountA.deposit(dkk100Money);
+//		The amount should be the same as before withdrawing since we subtract and add same amount of money
+		assertEquals(Double.valueOf(1000000),testAccountA.getBalance().getAmount());
 	}
 	
 	@Test
 	public void testGetBalance() {
-		fail("Write test case here");
+//		Creating test account
+		var testAccountC = new Account("Test", SEK);
+//		Creating money to deposit
+		var moneyToDeposit = new Money(100, SEK);
+//		Depositing some money to the account with the same currency
+		testAccountC.deposit(moneyToDeposit);
+//		Checking if currency of the balance is the same as initial currency with which account was created
+		assertEquals(testAccountC.getBalance().getCurrency().getName(), SEK.getName());
+//		Checking if amount of money is the same as was deposited initially
+		assertEquals(testAccountC.getBalance().getAmount(), moneyToDeposit.getAmount());
+
 	}
 }
--- individual-hw/task-6-7/test/b_Money/BankTest.java
+++ individual-hw/task-6-7/test/b_Money/BankTest.java
@@ -1,9 +1,9 @@
 package b_Money;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class BankTest {
 	Currency SEK, DKK;
@@ -16,49 +16,102 @@ public class BankTest {
 		SweBank = new Bank("SweBank", SEK);
 		Nordea = new Bank("Nordea", SEK);
 		DanskeBank = new Bank("DanskeBank", DKK);
-		SweBank.openAccount("Ulrika");
-		SweBank.openAccount("Bob");
-		Nordea.openAccount("Bob");
-		DanskeBank.openAccount("Gertrud");
+		SweBank.openAccount("Account0");
+		SweBank.openAccount("Account1");
+		Nordea.openAccount("Account0");
+		DanskeBank.openAccount("Account0");
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Write test case here");
+//		Checking if bank name is the same as it was in setUp function
+		assertEquals("SweBank",SweBank.getName());
+		assertEquals("Nordea",Nordea.getName());
 	}
 
 	@Test
 	public void testGetCurrency() {
-		fail("Write test case here");
+//		Checking if bank currency is the same as it was in setUp function
+		assertEquals(SEK, SweBank.getCurrency());
+//		Checking with different currency
+		assertEquals(DKK, DanskeBank.getCurrency());
 	}
 
 	@Test
 	public void testOpenAccount() throws AccountExistsException, AccountDoesNotExistException {
-		fail("Write test case here");
+//		Checking if new account can be created
+		var accountId = "TestAccount";
+		try {
+			SweBank.openAccount(accountId);
+		} catch(AccountExistsException e) {
+			fail("Account exist with such id");
+		}
+
+//		Checking if account with existed id can be created
+		try
+		{
+			SweBank.openAccount(accountId);
+			fail("Account was added");
+		}
+		catch (AccountExistsException e)
+		{
+			assertTrue("Account was no added successfully", true);
+		}
 	}
 
 	@Test
 	public void testDeposit() throws AccountDoesNotExistException {
-		fail("Write test case here");
+//		Adding money to account that was added in setUp function
+		SweBank.deposit("Account0", new Money(100, SEK));
+//		Checking if money were added
+		assertEquals(Double.valueOf(100), SweBank.getBalance("Account0"));
+//		Adding more money but with other currency
+		SweBank.deposit("Account0", new Money(100, DKK));
+//		Checking if money were added
+		assertEquals(Double.valueOf(233), SweBank.getBalance("Account0"));
 	}
 
 	@Test
 	public void testWithdraw() throws AccountDoesNotExistException {
-		fail("Write test case here");
+//		Adding money to account that was added in setUp function
+		SweBank.deposit("Account0", new Money(100, SEK));
+//		Checking if money were added
+		assertEquals(Double.valueOf(100), SweBank.getBalance("Account0"));
+//		Removing some money of the same currency
+		SweBank.withdraw("Account0", new Money(50, SEK));
+//		Checking if money were added
+		assertEquals(Double.valueOf(53), SweBank.getBalance("Account0"));
+//		Removing some money of the different currency
+		SweBank.withdraw("Account0", new Money(10, DKK));
+//		Checking if money with other currency was subtracted
+		assertEquals(Double.valueOf(40), SweBank.getBalance("Account0"));
 	}
 	
 	@Test
 	public void testGetBalance() throws AccountDoesNotExistException {
-		fail("Write test case here");
+//		Adding money to account that was added in setUp function
+		SweBank.deposit("Account0", new Money(100, SEK));
+//		Checking if money were added
+		assertEquals(Double.valueOf(100), SweBank.getBalance("Account0"));
 	}
 	
 	@Test
 	public void testTransfer() throws AccountDoesNotExistException {
-		fail("Write test case here");
-	}
-	
-	@Test
-	public void testTimedPayment() throws AccountDoesNotExistException {
-		fail("Write test case here");
+//		Adding money to accounts that were added in setUp function
+		SweBank.deposit("Account0", new Money(100, SEK));
+		SweBank.deposit("Account1", new Money(100, SEK));
+//		Transferring money
+		SweBank.transfer("Account0", "Account1", new Money(50, SEK));
+//		Checking if money were transferred
+		assertEquals(Double.valueOf(53), SweBank.getBalance("Account0"));
+		assertEquals(Double.valueOf(147), SweBank.getBalance("Account1"));
+
+//		Adding transferred previously money
+		SweBank.deposit("Account0", new Money(50, SEK));
+//		Transferring money to account from different bank
+		SweBank.transfer("Account0", Nordea, "Account0", new Money(50, SEK));
+//		Checking if money were transferred
+		assertEquals(Double.valueOf(53), SweBank.getBalance("Account0"));
+		assertEquals(Double.valueOf(47), Nordea.getBalance("Account0"));
 	}
 }
